29f8b9305453531598cb0e21ee4322ce83f77b63
Maciej Olejnik <maciej.olejnik88@gmail.com>
2016-11-23 15:53:47 +0100

    Ćwiczenie 13 Ustawienia zapisane w pliku zew .ini

--- toto2.py
+++ toto2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from totomodul import ustawienia, losujliczby, pobierztypy
+from totomodul import ustawienia, losujliczby, pobierztypy, wyniki
 
 # program główny
 
 # ustalmy trudność gry
-ileliczb, maksliczba, ilerazy = ustawienia()
+nick, ileliczb, maksliczba, ilerazy = ustawienia()
 
 # losujemy liczby
 liczby = losujliczby(ileliczb, maksliczba)
@@ -14,16 +14,6 @@ liczby = losujliczby(ileliczb, maksliczba)
 # pobieramy typy użytkownika i sprawdzamy ile liczb trafił
 		
 for i in range(ilerazy) :
+	wyniki(pobierztypy(ileliczb, maksliczba),liczby)
 	
-	typy = pobierztypy(ileliczb, maksliczba)
-	trafione = set(liczby) & typy
-
-	if trafione :
-		print("\nIlość trafień:", len(trafione))
-		print("Trafione liczby to :", str(trafione).strip('[]'))
-	else :
-		print("Brak trafień")
-	
-	print("\n" + "x"*40 + "\n")
-
 print("Wylosowane liczby : ",liczby)
--- totomodul.py
+++ totomodul.py
@@ -2,53 +2,98 @@
 # -*- coding: utf-8 -*-
 
 import random
+import os
 
 
 def ustawienia():
-	"""Funkcja pobiera ilość losowanych liczb, maksymalną losowaną
-	wartość oraz ilość prób. Pozwala określić stopień trudności gry."""
-	while 1:
-		try:
-			ile = int(input("Podaj ilość typowanych liczb: "))
-			maks = int(input("Podaj maksymalną losowaną liczbę: "))
-			if ile > maks:
-				print("Błędne dane!")
-				continue
-			ilelos = int(input("Ile losowań: "))
-			return (ile, maks, ilelos)
-		except:
-			print("Błędne dane!")
-			continue
-			
+    """Funkcja pobiera nick, ilość losowanych liczb, maksymalną losowaną
+	wartość oraz ilość prób. Pozwala określić stopień trudności gry.
+	Ustawienia są zapisywane w pliku"""
+
+    nick = input("Podaj nick: ")
+    nazwapliku = nick + ".ini"
+    gracz = czytaj_ust(nazwapliku)
+    odp = None
+    if gracz:
+        print("Twoje ustawienia:")
+        print("Liczb:", gracz[1])
+        print("Z Maks:", gracz[2])
+        print("Losowań:", gracz[3])
+        odp = input("Zmieniasz (t/n)?")
+
+    if not gracz or odp.lower() == "t":
+        while 1:
+            try:
+                ile = int(input("Podaj ilość typowanych liczb: "))
+                maks = int(input("Podaj maksymalną losowaną liczbę: "))
+                if ile > maks:
+                    print("Błędne dane!")
+                    continue
+                ilelos = int(input("Ile losowań: "))
+                break
+            except:
+                print("Błędne dane!")
+                continue
+        gracz = zapisz_ust(nazwapliku, [nick, str(ile), str(maks), str(ilelos)])
+
+    return gracz[0:1] + list(map(lambda x: int(x), gracz[1:4]))
+
+
+def czytaj_ust(nazwapliku):
+    if os.path.isfile(nazwapliku):
+        plik = open(nazwapliku, "r")
+        linia = plik.readline()
+        if linia:
+            return linia.split(";")
+    return False
+
+
+def zapisz_ust(nazwapliku, gracz):
+    plik = open(nazwapliku, "w")
+    plik.write(";".join(gracz))
+    plik.close()
+    return gracz
+
 
 def losujliczby(ile, maks):
-	#Funkcja losuje ile unikalnych liczb całkowitych od 1 do maks
-	liczby = []
-	i = 0
-	while i < ile:
-		liczba = random.randint(1, maks)
-		if liczby.count(liczba)==0:
-			liczby.append(liczba)
-			i = i + 1
-	return liczby
+    # Funkcja losuje ile unikalnych liczb całkowitych od 1 do maks
+    liczby = []
+    i = 0
+    while i < ile:
+        liczba = random.randint(1, maks)
+        if liczby.count(liczba) == 0:
+            liczby.append(liczba)
+            i = i + 1
+    return liczby
 
 
 def pobierztypy(ile, maks):
-	#Funkcja pobiera od użytkownika jego typy wylosowanych liczb
-	print("Wytypuj",ile,"z",maks," liczb")
-	typy=set()
-	i = 0
-	while i < ile:
-		try:
-			typ=int(input("Podaj liczbę "+str(i+1)+": "))
-		except ValueError:
-			print("Błędne dane!")
-			continue
-			
-		if 0 < typ <= maks and typ not in typy:
-			typy.add(typ)
-			i = i + 1
-	return typy
-	
+    # Funkcja pobiera od użytkownika jego typy wylosowanych liczb
+    print("Wytypuj", ile, "z", maks, " liczb")
+    typy = set()
+    i = 0
+    while i < ile:
+        try:
+            typ = int(input("Podaj liczbę " + str(i + 1) + ": "))
+        except ValueError:
+            print("Błędne dane!")
+            continue
 
-	
+        if 0 < typ <= maks and typ not in typy:
+            typy.add(typ)
+            i = i + 1
+    return typy
+
+
+def wyniki(typy, liczby):
+    '''Funkcja pobiera zbiór typów i listę liczb, wyznacza iloczyn
+	zbiorów i podaje ilość elementów wspólnych'''
+    trafione = set(liczby) & typy
+
+    if trafione:
+        print("\nIlość trafień:", len(trafione))
+        print("Trafione liczby to :", str(trafione).strip('[]'))
+    else:
+        print("Brak trafień")
+
+    print("\n" + "x" * 40 + "\n")
