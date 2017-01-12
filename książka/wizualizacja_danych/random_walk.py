53c75a23db463f1f4c96128de042737456acebed
Maciej Olejnik <maciej.olejnik88@gmail.com>
2017-01-12 16:30:57 +0100

    Dokończenie błądzenia losowego.

--- książka/wizualizacja_danych/random_walk.py
+++ książka/wizualizacja_danych/random_walk.py
@@ -19,6 +19,15 @@ class RandomWalk():
 		# Listy ze współrzędnymi punktów
 		self.x_values = [self.x_start]
 		self.y_values = [self.y_start]
+	
+	def get_step(self):
+		""""
+		Ustalenie kierunku oraz długości kroku.
+		"""
+		step_dir = choice([-1, 1])
+		step_dist = choice(list(range(5))) 
+		step = step_dir * step_dist
+		return step
 		
 	def fill_walk(self):
 		"""Wygenerowanie wszystkich punktów dla błądzenia losowego."""
@@ -28,13 +37,10 @@ class RandomWalk():
 		while len(self.x_values) < self.num_points:
 			# Ustalenie kierunku oraz odległości do pokonania w tym
 			# kierunku
-			x_direction = choice([-1, 1])
-			x_distance = choice([0, 1, 2, 3, 4])
-			x_step = x_direction * x_distance
 			
-			y_direction = choice([-1, 1])
-			y_distance = choice([0, 1, 2, 3, 4])
-			y_step = y_direction * y_distance
+			x_step = self.get_step()
+			
+			y_step = self.get_step()
 			
 			# Odrzucenie ruchów, które prowadzą donikąd
 			if x_step == 0 and y_step == 0:
--- książka/wizualizacja_danych/rw_visual.py
+++ książka/wizualizacja_danych/rw_visual.py
@@ -8,10 +8,46 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-	rw = RandomWalk()
+	rw = RandomWalk(50000)
 	rw.fill_walk()
+	settings = {
+			'x' : rw.x_values,
+			'y' : rw.y_values,
+			's' : 1,
+			'c' : list(range(rw.num_points)),
+			'cmap' : 'Accent',
+			'edgecolor' : 'None',
+			}
+	
+	# ustawienia punktu początkowego
+	settings_0 = {
+			'x' : rw.x_values[0],
+			'y' : rw.y_values[0],
+			's' : 100,
+			'c' : 'green',
+			'edgecolor' : 'None',
+			}
+	
+	# ustawienia punktu końcowego
+	settings_end = {
+			'x' : rw.x_values[-1],
+			'y' : rw.y_values[-1],
+			's' : 100,
+			'c' : 'red',
+			'edgecolor' : 'None',
+			}
+			
+	# określenie wielkości okna wykresu
+	plt.figure(figsize=(10, 6), dpi=128)
 
-	plt.scatter(rw.x_values, rw.y_values, s=15)
+	plt.scatter(**settings)
+	plt.scatter(**settings_0)
+	plt.scatter(**settings_end)
+	
+	# ukrycie osi
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
+	
 	plt.show()
 	
 	keep_running = input("Jeszcze raz? T/n : ")
